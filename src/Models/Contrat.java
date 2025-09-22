2703dc1617574908f053e3ac21b8539eb83b598b
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-22 11:00:40 +0100

    here i add the logic of the model Contract nalso i mentioned the type of it

--- src/Models/Contrat.java
+++ src/Models/Contrat.java
@@ -1,5 +1,50 @@
 package Models;
 
 public class Contrat {
+    private int id;
+    private String dateDebut;
+    private String dateFin;
+    private enum TypeContrat{
+        AUTOMOBILE,
+        MALADI,
+        IMOBILITIE,
+    }
+    private TypeContrat typeContrat;
 
+    public Contrat(int id, String dateDebut, String dateFin, TypeContrat typeContrat) {
+        this.id = id;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.typeContrat = typeContrat;
+    }
+    public Contrat() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(String dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public String getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(String dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public TypeContrat getTypeContrat() {
+        return typeContrat;
+    }
+
+    public void setTypeContrat(TypeContrat typeContrat) {
+        this.typeContrat = typeContrat;
+    }
 }
