abad28ff34dcbe3864f1c6eb35c89d1d3098168c
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-22 13:27:03 +0100

    here i add the service of client

--- src/DAOs/ClientDAO.java
+++ src/DAOs/ClientDAO.java
@@ -1,4 +1,27 @@
 package DAOs;
 
+import Models.Client;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ClientDAO {
+
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    public ClientDAO(Connection connection) {
+         this.connection = connection;
+    }
+    public void addClient(Client client) {
+        try{
+            String sql = "INSERT INTO client VALUES (?,?,?,?,?,?,?)";
+            preparedStatement=connection.prepareStatement(sql);
+        }
+        catch(NullPointerException | SQLException e){
+            throw new RuntimeException("couldnt add client because" + e.getMessage());
+        }
+
+
+    }
 }
