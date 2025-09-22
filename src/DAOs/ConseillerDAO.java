a396409670c4e124e094ac1c6251947d0114f7d5
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-22 16:03:22 +0100

    here i add the ConseillerDAO

--- src/DAOs/ConseillerDAO.java
+++ src/DAOs/ConseillerDAO.java
@@ -1,4 +1,30 @@
 package DAOs;
 
+import Config.DbConnextion;
+import Models.Conseiller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class ConseillerDAO {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private String drivers = "com.mysql.cj.jdbc.Driver";
+    public ConseillerDAO() {
+        this.connection = new DbConnextion().getConnection();
+    }
+
+    public boolean addConseiller(Conseiller conseiller) {
+        String sql = "INSERT INTO conseiller (nom, prenom, email, telephone) VALUES (?, ?, ?, ?)";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, conseiller.getNom());
+            preparedStatement.setString(2, conseiller.getPrenom());
+            preparedStatement.setString(3, conseiller.getEmail());
+            preparedStatement.setString(4, conseiller.getTelephone());
+            return preparedStatement.executeUpdate() > 0;
+        } catch (Exception e) {
+            throw new RuntimeException("erreur lors de l'ajout du conseiller car" + e.getMessage());
+        }
+    }
 }
