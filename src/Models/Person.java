e7a90b1f5b9e83bf5f1301232527f0d3e41a2655
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-22 09:57:04 +0100

    here i created the project + i create my first model class

---
+++ src/Models/Person.java
@@ -0,0 +1,72 @@
+package Models;
+
+public abstract class Person {
+    protected int id;
+    protected String nom;
+    protected String prenom;
+    protected String email;
+    protected String adresse;
+    protected String telephone;
+    protected String role;
+    public Person(String nom, String prenom, String email, String adresse, String telephone, String role) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.adresse = adresse;
+        this.telephone = telephone;
+        this.role = role;
+    }
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
