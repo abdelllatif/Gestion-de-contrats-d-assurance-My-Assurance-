ee3a1904df33096fb1330889c9429b744c54fd34
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-22 10:03:29 +0100

    here i add the class of Client  respecting the heritage

--- src/Models/Client.java
+++ src/Models/Client.java
@@ -1,4 +1,30 @@
 package Models;
 
-public class Client {
+import java.util.HashMap;
+
+public class Client extends Person{
+    private HashMap<Integer, String> contrats;
+    private String conseiller;
+    public Client(String nom, String prenom, String email, String adresse, String telephone, String role, HashMap<Integer, String> contrats, String conseiller) {
+        super(nom, prenom, email, adresse, telephone, role);
+        this.contrats = contrats;
+        this.conseiller = conseiller;
+    }
+    public Client() {
+    }
+    public HashMap<Integer, String> getContrats() {
+        return contrats;
+    }
+
+    public void setContrats(HashMap<Integer, String> contrats) {
+        this.contrats = contrats;
+    }
+
+    public String getConseiller() {
+        return conseiller;
+    }
+
+    public void setConseiller(String conseiller) {
+        this.conseiller = conseiller;
+    }
 }
