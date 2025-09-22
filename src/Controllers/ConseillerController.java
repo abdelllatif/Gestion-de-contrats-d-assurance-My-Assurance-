20b9922b41e8f298f7da9d32f544b7428cdff0a9
Adellatif Hissoune <haissouneabdellatif749@gmail.com>
2025-09-22 16:11:03 +0100

    here i add the the Controller  of the conseiller

--- src/Controllers/ConseillerController.java
+++ src/Controllers/ConseillerController.java
@@ -1,4 +1,13 @@
 package Controllers;
 
+import Models.Conseiller;
+import Services.ConseillerService;
+
 public class ConseillerController {
+    public ConseillerController() {
+    }
+    public void addConseiller(Conseiller conseiller) {
+        ConseillerService conseillerService = new ConseillerService();
+        conseillerService.addConseiller(conseiller);
+    }
 }
